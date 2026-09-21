d020e620dc29c4f63a34ddfe97b321021e5843d5
Eman9202 <jodi1690@hotmail.com>
2026-09-21 22:48:16 +0200

    Deploy localized meals and workouts

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='mizan-v18-i18n-onboarding';
+const CACHE='mizan-v19-meals-workouts-i18n';
 const CORE=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).catch(()=>{}));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('mizan-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
