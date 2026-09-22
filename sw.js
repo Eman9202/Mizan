5dc0be08404393dd5c771b73b6c66a021c34c597
Eman9202 <jodi1690@hotmail.com>
2026-09-22 11:08:07 +0200

    Refresh PWA cache for multilingual chat flow fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='mizan-v24-fresh-ui';
+const CACHE='mizan-v25-chat-flow';
 const CORE=['./','./index.html','./manifest.webmanifest','./assets/locales/sv-bs.js','./assets/i18n.js'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).catch(()=>{}));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('mizan-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
